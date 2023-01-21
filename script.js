e279576f0ca768f839b33029a872e5e0bf986f34
sabanansari <alisaban52@gmail.com>
2023-01-22 01:31:44 +0530

    save favorite to local storage fn added

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
 function updateDOM(){
     resultsArray.forEach((result)=>{
@@ -40,6 +41,7 @@ function updateDOM(){
         const favorites = document.createElement('p');
         favorites.classList.add('clickable');
         favorites.textContent = 'Add To Favorites';
+        favorites.setAttribute('onclick',`saveFavorite('${result.url}')`);
         // Card Text
         const cardText = document.createElement('p');
         cardText.classList.add('card-text');
@@ -75,6 +77,24 @@ async function getNasaPictures(){
     } catch (error) {
         console.log(error);
     }
+}
+
+// Add result to Favorites
+function saveFavorite(itemUrl){
+    // Loop through Results Array to select Favorite
+    resultsArray.forEach((item)=>{
+        if(item.url.includes(itemUrl) && !favorites[itemUrl]){
+            favorites[itemUrl] = item;
+
+            // Show Save Confirmation for 2 secs
+            saveConfirmed.hidden = false;
+            setTimeout(()=>{
+                saveConfirmed.hidden = true;
+            },2000);
+            // Set Favorites in localStorage
+            localStorage.setItem('nasaFavorites',JSON.stringify(favorites));
+        }
+    });
 }
 
 // On Load
